e8377ea24882f939247277679cc05ffede30e6ea
Dorian Staten <dastaten@gmail.com>
2023-02-23 14:53:18 -0600

    config logic done

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,22 @@
+const players = [
+  {
+    name: '',
+    symbol: 'X',
+  },
+  {
+    name: '',
+    symbol: 'O',
+  },
+];
+
+let editedPlayer = 0;
+
+document.getElementById('playername').value = '';
+
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.getElementById('config-form');
+const errorOutputElement = document.getElementById('config-error');
 
 const editPlayer1ButtonElement = document.getElementById('edit-player-1-btn');
 const editPlayer2ButtonElement = document.getElementById('edit-player-2-btn');
--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid;
   playerConfigOverlayElement.style.display = 'block';
   backdropElement.style.display = 'block';
 }
@@ -6,10 +7,26 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorOutputElement.textContent = '';
+  document.getElementById('playername').value = '';
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredPlayerName = formData.get('playername');
+  const enteredPlayerName = formData.get('playername').trim();
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add('error');
+    errorOutputElement.textContent = 'Please enter a valid name!';
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
